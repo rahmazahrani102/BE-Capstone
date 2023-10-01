65934fe84bbfe877e6bdab0a00941ef2e7db3e07
OchaMeilyla <ochameilyla@gmail.com>
2023-10-01 09:00:24 +0700

    Database Connected

--- app/models/ProgramModels.js
+++ app/models/ProgramModels.js
@@ -4,6 +4,7 @@ const mysql = require('mysql2')
 const connection = new sequelize.Sequelize ('eduspace', 'avnadmin','AVNS_obABAi0slRro6rNh6P4',{
     host: 'mysql-377d93a-rahmazahrani102-5e6f.aivencloud.com',
     dialect: 'mysql',
+    port: '11858',
     logging: false
 })
 
